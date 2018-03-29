0f6a9c7cf194a9841e7c0fa7aeb99a2f00b21465
Daniel Hines <d4hines@gmail.com>
2018-03-29 07:21:45 -0400

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject dynamics-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.microsoft.azure/adal4j "1.4.0"]
+                 [ring/ring-codec "1.1.0"]
+                 [clj-http "3.8.0"]
+                 [cheshire "5.8.0"]
+                 [cheshire "5.8.0"]])
---
+++ src/dynamics_clj/core.clj
@@ -0,0 +1,38 @@
+(ns dynamics-clj.core
+  (:require [clojure.string :as str]
+            [ring.util.codec :refer [url-encode]]
+            [clj-http.client :as client]))
+
+(def config {:crmorg "https://d4hines.crm.dynamics.com"
+             :clientid "020a1b54-a7d4-4732-8efc-01c13adf8c0c"
+             :username "d4hines@d4hines.onmicrosoft.com"
+             :password "YechiGlihyechu8"
+             :tokenendpoint "https://login.microsoftonline.com/94348c85-35ff-4ed8-aecd-61b85d0f4c17/oauth2/token"
+             :crmwebapihost "d4hines.api.crm.dynamics.com"
+             :crmwebapipath "/api/data/v9.0/contacts"})
+
+(def utf-8-octet-length #(-> % (.getBytes "UTF-8") count))
+
+(defn request
+  "I don't do a whole lot."
+  [{:keys [crmorg clientid username password
+           tokenendpoint crmwebapihost crmwebapipath]}]
+  (let [split-end (-> tokenendpoint str/lower-case (str/replace #"https:\/\/" "") (str/split #"/"))
+        authhost (first split-end)
+        authpath (str "/" (->> split-end rest (str/join "/")))
+        reqstring (str "client_id=" clientid
+                       "&resource=" (url-encode crmorg)
+                       "&username=" (url-encode username)
+                       "&password=" (url-encode password)
+                       "&grant_type=password")
+        tokenrequestoptions {:host authhost
+                             :path authpath
+                             :method "POST"
+                             :headers {"Content-Type" "application/x-www-form-urlencoded"
+                                        "Content-Length" (utf-8-octet-length reqstring)}}]
+    tokenrequestoptions))
+
+
+(request config)
+(client/get "http://jsonplaceholder.typicode.com/posts" {:as :json})
+
