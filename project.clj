f3dbebab3556d0e95f69b37bec684cf76d0fbb75
Daniel Hines <d4hines@gmail.com>
2018-04-08 02:17:42 -0400

    Finished all features except images

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynamics-clj "0.1.1-SNAPSHOT"
+(defproject dynamics-clj "0.1.4-SNAPSHOT"
   :description "Dynamics CRM Web API helper in Clojure"
   :url "https://github.com/d4hines/dynamics-clj"
   :license {:name "Eclipse Public License"
